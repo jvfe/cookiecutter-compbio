20a03c7117d7a9cc346e87dd9b6e9be61c0fa9fc
jvfe <jvfecav@gmail.com>
2020-07-28 17:02:20 -0300

    Add additional directories

---
+++ {{cookiecutter.project_slug}}/src/helpers/utils.py
@@ -0,0 +1,13 @@
+"""Module containing functions of common use across your analyses"""
+
+def example(statement):
+    """Example function
+
+    Args:
+        statement (str): A string containing a statement.
+
+    Returns:
+        The same statement.
+    """
+
+    return print(statement)
